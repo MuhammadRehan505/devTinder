6941961a88123c6400721f9e0eed270d58203a14
Rehan <Uijaz9159@gmail.com>
2026-02-03 13:33:26 +0500

    new concept(how js works runtime)

--- src/app.js
+++ src/app.js
@@ -2,9 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1st Response");
+    next();
+    res.send("1st response");
+  },
+  (req, res, next) => {
+    console.log("2nd Response");
+    next();
+    res.send("2nd response");
+  },
+);
 
 app.listen(3000, () => {
   console.log("SERVER IS LISTENING!!!");
